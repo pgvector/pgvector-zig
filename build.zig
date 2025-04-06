dce9afe99dfc83d9f930649c02a53d776e7a35a5
Andrew Kane <andrew@ankane.org>
2025-04-05 20:59:56 -0700

    Removed unneeded code

--- build.zig
+++ build.zig
@@ -6,8 +6,6 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("examples/pg.zig"),
         .target = b.graph.host,
     });
-    pgExe.linkSystemLibrary("pq");
-    pgExe.linkLibC();
     const pg = b.dependency("pg", .{});
     pgExe.root_module.addImport("pg", pg.module("pg"));
     b.installArtifact(pgExe);
